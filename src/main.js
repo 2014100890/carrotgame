a1355917221dc738b97f2049bd0461cb7674511b
2014100890 <2014100890@khu.ac.kr>
2021-07-05 13:54:10 +0900

    Refactoring(field, popup)

--- src/main.js
+++ src/main.js
@@ -1,21 +1,15 @@
 'use strict'
+import PopUp from './popup.js'
+import Field from './field.js'
 
-const CARROT_SIZE = 80
 const CARROT_COUNT = 5
 const BUG_COUNT = 5
 const GAME_DURATION_SEC = 5
 
-const field = document.querySelector('.game__field')
-const fieldRect = field.getBoundingClientRect()
 const gameBtn = document.querySelector('.game__button')
 const gameTimer = document.querySelector('.game__timer')
 const gameScore = document.querySelector('.game__score')
 
-const popUp = document.querySelector('.pop-up')
-const popUpMessgae = document.querySelector('.pop-up__message')
-const popUpRefresh = document.querySelector('.pop-up__refresh')
-
-const carrotSound = new Audio('./sound/carrot_pull.mp3')
 const alertSound = new Audio('./sound/alert.wav')
 const bgSound = new Audio('./sound/bg.mp3')
 const bugSound = new Audio('./sound/bug_pull.mp3')
@@ -25,7 +19,29 @@ let started = false
 let score = 0
 let timer = undefined
 
-field.addEventListener('click', onFieldClick)
+const gameFinishBanner = new PopUp()
+
+gameFinishBanner.setClickListener(() => {
+  startGame()
+})
+
+const gameField = new Field(CARROT_COUNT, BUG_COUNT)
+gameField.setClickListener(onItemClick)
+
+function onItemClick(item) {
+  if (!started) {
+    return
+  }
+  if (item === 'carrot') {
+    score++
+    updateScoreBoard()
+    if (score === CARROT_COUNT) {
+      finishGame(true)
+    }
+  } else if (item === 'bug') {
+    finishGame(false)
+  }
+}
 
 gameBtn.addEventListener('click', () => {
   if (started) {
@@ -35,11 +51,11 @@ gameBtn.addEventListener('click', () => {
   }
 })
 
-popUpRefresh.addEventListener('click', () => {
-  startGame()
-  hidePopUp()
-})
-
+function initGame() {
+  score = 0
+  gameScore.innerText = CARROT_COUNT
+  gameField.init()
+}
 function startGame() {
   started = true
   initGame()
@@ -53,7 +69,7 @@ function stopGame() {
   started = false
   stopGameTimer()
   hideGameButton()
-  showPopUpWithText('REPLAY?')
+  gameFinishBanner.showWithText('REPLAY?')
   playSound(alertSound)
   stopSound(bgSound)
 }
@@ -68,7 +84,7 @@ function finishGame(win) {
   }
   stopGameTimer()
   stopSound(bgSound)
-  showPopUpWithText(win ? 'YOU WON  ❤️' : 'YOU LOST')
+  gameFinishBanner.showWithText(win ? 'YOU WON  ❤️' : 'YOU LOST')
 }
 
 function showStopButton() {
@@ -110,48 +126,6 @@ function updateTimerText(time) {
   gameTimer.innerText = `${minutes}:${seconds}`
 }
 
-function showPopUpWithText(text) {
-  popUpMessgae.innerText = text
-  popUp.classList.remove('pop-up--hide')
-}
-
-function hidePopUp() {
-  popUp.classList.add('pop-up--hide')
-}
-
-function initGame() {
-  score = 0
-  field.innerHTML = ''
-  gameScore.innerHTML = CARROT_COUNT
-  // 벌레와 당근을 생성한 뒤 field에 추가한다.
-  addItem('carrot', CARROT_COUNT, 'img/carrot.png')
-  addItem('bug', BUG_COUNT, 'img/bug.png')
-}
-
-function onFieldClick(event) {
-  console.log(event)
-  if (!started) {
-    return
-  }
-  const target = event.target
-  if (target.matches('.carrot')) {
-    target.remove()
-    score++
-    playSound(carrotSound)
-    updateScoreBoard()
-    if (score === CARROT_COUNT) {
-      finishGame(true)
-    }
-  } else if (target.matches('.bug')) {
-    finishGame(false)
-  }
-}
-
-function playSound(sound) {
-  sound.currentTime = 0
-  sound.play()
-}
-
 function stopSound(sound) {
   sound.pause()
 }
@@ -160,28 +134,7 @@ function updateScoreBoard() {
   gameScore.innerText = CARROT_COUNT - score
 }
 
-function addItem(className, count, imgPath) {
-  const x1 = 0
-  const y1 = 0
-  const x2 = fieldRect.width - CARROT_SIZE
-  const y2 = fieldRect.height - CARROT_SIZE
-
-  for (let i = 0; i < count; i++) {
-    const item = document.createElement('img')
-    item.setAttribute('class', className)
-    item.setAttribute('src', imgPath)
-    item.style.position = 'absolute'
-
-    const x = randomNumber(x1, x2)
-    const y = randomNumber(y1, y2)
-
-    item.style.left = `${x}px`
-    item.style.top = `${y}px`
-
-    field.appendChild(item)
-  }
-}
-
-function randomNumber(min, max) {
-  return Math.random() * (max - min) + min
+function playSound(sound) {
+  sound.currentTime = 0
+  sound.play()
 }
